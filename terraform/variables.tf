8d9e2860c5e782189dddc8f2e972a85974978bf1
rutuja-2199 <rutujab2199@gmail.com>
2026-02-05 18:01:33 +0530

    Creating VM on GCP

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  default     = "devops-blog"
+}
+
+variable "region" {
+  description = "GCP region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  default     = "us-central1-a"
+}
+
+variable "vm_name" {
+  description = "Name of the VM"
+  default     = "devops-blog-vm"
+}
+
+variable "machine_type" {
+  description = "VM machine type"
+  default     = "e2-medium"
+}
+
+variable "network_tags" {
+  description = "Network tags for firewall rules"
+  type        = list(string)
+  default     = ["devops-blog"]
+}
